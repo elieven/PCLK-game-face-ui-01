d27b9cc7479bcaef55cfc559aadf2df380a71512
Elieven <elieven@protonmail.com>
2023-01-08 20:48:38 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -141,6 +141,10 @@ const CurrentTimeIndicator = () => {
   return <Stat label={areaContent.currentTime.label} value={time} />;
 };
 
+const BottomControls = () => {
+  return <div></div>
+}
+
 const PokerGameFace = () => {
   return (
     <div className="h-screen flex flex-col p-4 space-y-4 font-bold">
